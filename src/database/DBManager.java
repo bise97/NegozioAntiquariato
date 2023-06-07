4d3e4f2e6821ab493d0f48d4002f00eca8edb4cc
Gaetano <gaetano.martedi1@studenti.unicampania.it>
2023-06-07 15:42:44 +0200

    Created database and DBManager

---
+++ src/database/DBManager.java
@@ -0,0 +1,33 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    private final static String DB_PATH = "jdbc:h2:NegozioAntiquariato";
+    private final static String DB_USERNAME = "";
+    private final static String DB_PASSWORD = "";
+    private static Connection connection = null;
+
+    public static Connection getConnection()  throws SQLException {
+        if(connection == null){
+            connection = DriverManager.getConnection(DB_PATH,DB_USERNAME,DB_PASSWORD);
+            createTables();
+        }
+        if(connection == null || connection.isClosed()){
+            connection = DriverManager.getConnection(DB_PATH,DB_USERNAME,DB_PASSWORD);
+        }
+        return connection;
+    }
+
+    private static void createTables(){
+
+    }
+
+    public static void closeConnection() throws SQLException{
+        if(connection!=null){
+            connection.close();
+        }
+    }
+}
