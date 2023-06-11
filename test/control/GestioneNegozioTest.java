31e7b054b46255f05f2fa11e8733bcff547a7f3e
Gaetano <gaetano.martedi1@studenti.unicampania.it>
2023-06-11 17:33:24 +0200

    Create test for modificaArticolo

--- test/control/GestioneNegozioTest.java
+++ test/control/GestioneNegozioTest.java
@@ -1,14 +1,33 @@
 package control;
 
+import boundary.BGestore;
+import database.ArticoloDAO;
+import database.DBSetup;
+import entity.Articolo;
+import exception.DAOConnectionException;
+import exception.DAOException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GestioneNegozioTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        try{
+            DBSetup.initialize();
+        }
+        catch (SQLException e){
+            fail("Database non inizializzato");
+        }
+
+    }
 
     @BeforeEach
     void setUp() {
@@ -20,6 +39,36 @@ class GestioneNegozioTest {
 
     @Test
     void modificaArticolo() {
+        long codiceArticolo = 1L;
+        BGestore bGestore = new BGestore();
+        String prezzo = "0.5\n";
+        String quantitaMagazzino = "10\n";
+        String nome = "Cane\n";
+        String descrizione = "Sierra\n";
+        String vuoiModificareImmagini = "n\n";
+
+
+        Articolo articolo = null;
+        try{
+            articolo = ArticoloDAO.readArticolo(codiceArticolo);
+        }catch (DAOException | DAOConnectionException e){
+            fail("Errore nella lettura dell'articolo dal database");
+        }
+
+        assertNotEquals(articolo,null); //test della pre-condizione
+
+        GestioneNegozio.getInstance().modificaArticolo(codiceArticolo, bGestore);
+
+        articolo = null;
+        try{
+            articolo = ArticoloDAO.readArticolo(codiceArticolo);
+        }catch (DAOException | DAOConnectionException e){
+            fail("Errore nella lettura dell'articolo dal database");
+        }
+
+        assertNotEquals(articolo,null);
+        assertEquals(articolo.getPrezzo(),Integer.parseInt(prezzo));
+        assertEquals(articolo.getQuantitaMagazzino(),Integer.parseInt(quantitaMagazzino));
     }
 
     @Test
